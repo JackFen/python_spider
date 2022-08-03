5cff0520dbae7f9281611e634601f8991b7548d5
JackFen <fengzhanweia@163.com>
2022-08-03 10:51:46 +0800

    完结撒花
    该项目实现了电影展现，词频统计并生成统计图以及词云树

--- douban_flask/app.py
+++ douban_flask/app.py
@@ -1,12 +1,26 @@
 from flask import Flask,render_template
 import sqlite3
-
+import jieba  #分词
 app = Flask(__name__)
 
 
 @app.route('/') #定义一个路径
 def index():  # put application's code here
-    return render_template("index.html")
+    # 准备词云所需要的文字
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = 'select introduction from movie250'
+    data = cur.execute(sql)
+    text = ""
+    for item in data:
+        text = text + item[0]
+    # print(text)
+
+    # 分词
+    cut = jieba.cut(text)
+    string = ' '.join(cut)  # 将cut中的每一个成员用' '空格分割
+    length=len(string)
+    return render_template("index.html",length=length)
 
 @app.route('/index')
 def home():
@@ -28,7 +42,18 @@ def movie():
 
 @app.route('/score')
 def score():
-    return render_template("score.html")
+    score=[] #评分
+    num=[]   #每个评分所统计出的电影数量
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(str(item[0]))
+        num.append(item[1])
+    cur.close()
+    con.close()
+    return render_template("score.html",score=score,num=num)
 
 @app.route('/word')
 def word():
---
+++ douban_flask/testCloud.py
@@ -0,0 +1,46 @@
+# -*- codeing = utf-8 -*-
+# @Time : 2022-08-03 上午 9:12
+# @Author : fengzhanwei
+# @File : testCloud.py
+# @Software : PyCharm
+
+import jieba  #分词
+from matplotlib import pyplot as plt #绘图，数据可视化
+from wordcloud import WordCloud      #词云
+from PIL import Image                #图片处理
+import numpy as np                   #矩阵运算
+import sqlite3                       #数据库
+
+# 准备词云所需要的文字
+con=sqlite3.connect("movie.db")
+cur=con.cursor()
+sql='select introduction from movie250'
+data = cur.execute(sql)
+text=""
+for item in data:
+    text=text+item[0]
+# print(text)
+
+# 分词
+cut=jieba.cut(text)
+string=' '.join(cut) #将cut中的每一个成员用' '空格分割
+# print(len(string))
+
+img=Image.open(r'.\static\assets\img\tree.jpg')
+img_array=np.array(img)  #将图片转换为数组
+wc=WordCloud(
+    background_color='white',
+    mask=img_array,
+    font_path="msyh.ttc" #字体所在位置：c:\windows\Fonts
+)
+wc.generate_from_text(string) #需要切好的词
+
+# 绘制图片
+fig=plt.figure(1)
+plt.imshow(wc)
+plt.axis('off') #是否显示坐标轴
+
+# plt.show() #显示生成的词云图片
+
+#输出词云图片到文件
+plt.savefig(r'.\static\assets\img\word.jpg',dpi=500) #dpi指的是清晰度
